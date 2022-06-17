c4848f156a172b99cf588d6ec01598e3a23605e5
cuongkq5555 <khuongquycuong@gmail.com>
2022-06-17 20:00:31 +0700

    ExamAngular

---
+++ src/app/Examangular/examangular.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from "@angular/core";
+
+
+
+@Component({
+  selector:"app-examangular",
+  templateUrl:"./examangular.component.html"
+})
+export class ExamangularComponent{
+  // data : IExam2[] = [
+  //   {Title: 'Sale Representive 1', HireDate: '10/05/1996', Adress: '7 Houndstooth Rd.', City:'London', Phone:'(71) 555-4444'},
+  // ];
+}
+
+
---
+++ src/app/interfaces/examangular.interface.ts
@@ -0,0 +1,7 @@
+export interface IContact {
+  Title: string;
+  HireDate: string;
+  Address:string;
+  City: string;
+  Phone: string;
+}
